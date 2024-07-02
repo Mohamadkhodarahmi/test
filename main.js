96437c54971861b9e2aba678452a74b0e0ebe145
mohamad <mohamad.epic.k@gmail.com>
2024-07-02 12:15:09 +0330

    add deleting function

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const animalElement = document.getElementById('animals');
-const animals = ["dog", "cat", "lion"];
+let index = ' ';
+let box = [];
+const animals = [
+    {
+        name : "dog",
+        index :1
+    }
+    ,
+    {
+        name :"cat" ,
+        index :2
+    }
+    ,
+    {
+        name : "lion",
+        index :3
+    }
+    ,
+    {
+        name :"zibra",
+        index :4
+    }
+];
 
-function template(animals) {
+
+function template(animals  ) {
     let data = '';
-   animals.forEach((animal) => {
-     data += `<div class="col-md-4 p-lg-4" >
+   animals.forEach((animal,key ) => {
+     data += `<div class="col-md-4 p-lg-4" data-index="${animal.index}" onclick="deleting(${animal.index})"  " >
             <div class="card p-3 bg-white">
-                <img src="" class="rounded" alt="cinque terre">
-                <h1 >${animal}</h1>
+                <img src="paris.jpg" class="rounded" alt="cinque terre">
+                <h1 >${animal.name}</h1>
 
                 <p style="padding: 4px">Something short and leading about the collection below—its contents, the creato</p>
                 <div class="btn-group col-1 pb-2">
@@ -19,9 +42,12 @@ function template(animals) {
         </div>`
 
    });
+    box = animals;
     animalElement.innerHTML = data;
-}
 
+
+}
+template(animals);
 document.getElementById('input').addEventListener('input', (event) => {
     const filterValue = event.target.value;
     const newAnimal = [];
@@ -30,8 +56,20 @@ document.getElementById('input').addEventListener('input', (event) => {
        if(animal.includes(filterValue))
            newAnimal.push(animal)
     });
-
     template(newAnimal);
-});
 
-template(animals);
+  })
+const deleting = (index) => {
+    box = box.filter((animal) => {
+        return animal.index !== index;
+    })
+    template(box);
+}
+
+
+
+
+
+
+
+
